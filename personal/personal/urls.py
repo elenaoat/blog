1190eaee9727b171524a06daca59a90e1b509f16
Elena <oat.elena@gmail.com>
2015-02-18 18:20:19 +0200

    add url for home view

--- personal/personal/urls.py
+++ personal/personal/urls.py
@@ -2,11 +2,12 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
-urlpatterns = patterns('',
+from blogengine import views
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'personal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
