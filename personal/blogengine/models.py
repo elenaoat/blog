ea0858c2698bae61f6171453ab34b8b4f90595b6
Elena <oat.elena@gmail.com>
2015-02-18 18:18:41 +0200

    add Post model

--- personal/blogengine/models.py
+++ personal/blogengine/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
-	title = models.CharField(max_length=200)
-	pub_date = models.DateTimeField()
-	text = models.TextField()
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField(default=timezone.now())
+    text = models.TextField()
+
+    def __unicode__(self):
+        return self.title
