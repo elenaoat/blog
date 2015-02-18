70e36355eb38d48eab57ab4c93f9e082f240c498
Elena <oat.elena@gmail.com>
2015-02-18 18:19:09 +0200

    add home view

--- personal/blogengine/views.py
+++ personal/blogengine/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+def home(request):
+    last_5 = models.Post.objects.order_by('-pub_date')[:5]
+    ctx = {'posts': last_5}
+
+    return render(request, 'home.html', ctx)
+
